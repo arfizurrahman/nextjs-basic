440641956df7ee951cf73bfdfaf3351966d28b0a
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-03-28 10:42:58 +0600

    Implementing custorm error page and getInitialProps hook

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import Link from 'next/link';
-import Router from 'next/router';
+import React, { Component } from "react";
+import Link from "next/link";
+import Router from "next/router";
 
-const indexPage = () => (
-    <div>
-        <h1>The Main Page</h1>
-        <p>Goo to <Link href="/auth"><a>Auth</a></Link></p>
-        <button onClick={() => Router.push('/auth')}>Go to Auth</button>
-    </div>
-);
+class IndexPage extends Component {
+    // static async getInitialProps(context) {
+    //     console.log(context);
+    //     return {appName: 'Next App'};
+    // }
+    static getInitialProps(context) {
+        console.log(context);
+        const promise = new Promise((resolve, reject) => {
+            setTimeout(() =>{
+                resolve({appName: 'Next App'});
+            }, 1000)
+        })
+        // promise.then();
+        return promise;
+    }
+    render() {
+        return (
+            <div>
+                <h1>The Main Page {this.props.appName}</h1>
+                <p>
+                    Go to{" "}
+                    <Link href="/auth">
+                        <a>Auth</a>
+                    </Link>
+                </p>
+                <button onClick={() => Router.push("/auth")}>Go to Auth</button>
+            </div>
+        );
+    }
+}
 
-export default indexPage;
+export default IndexPage;
