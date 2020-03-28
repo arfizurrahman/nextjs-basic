8bb195afd65da62603350fe626832808deb9dffd
Md. Arfizur Rahman <arfizrahman0@gmail.com>
2020-03-28 10:13:19 +0600

    Styling using next js

--- components/User.js
+++ components/User.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
 
-const user = (props) => (
+const user = props => (
     <div>
         <h1>{props.name}</h1>
         <p>Age: {props.age}</p>
+        <style jsx>{`
+            div {
+                border: 1 px solid #eee;
+                box-shadow: 0 2px 3px #ccc;
+                padding: 30px;
+                text-align: center;
+            }
+        `}</style>
     </div>
 );
 
